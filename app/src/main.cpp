3ec02b94b2e6b342f5bb3ccfe7054af13a4c9c12
xavi-b <developer@xavi-b.fr>
2020-08-23 15:37:08 +0200

    QML logs to XB::Log

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -7,6 +7,28 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
 
+    qInstallMessageHandler([](QtMsgType type, QMessageLogContext const& context, QString const& msg)
+    {
+        XB::LogLevel level;
+        switch (type)
+        {
+            case QtInfoMsg:
+                level = XB::LogLevel::INFO;
+                break;
+            case QtWarningMsg:
+                level = XB::LogLevel::WARN;
+                break;
+            case QtCriticalMsg:
+            case QtFatalMsg:
+                level = XB::LogLevel::ERROR;
+                break;
+            default:
+                level = XB::LogLevel::DEBUG;
+                break;
+        }
+        XB::Logger::log(level, QString("[QT CONSOLE] %1%2").arg(context.category ? QString("(%1) ").arg(context.category) : "").arg(msg));
+    });
+
     QApplication app(argc, argv);
 
     app.setOrganizationName(ORG_NAME);
